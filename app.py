6a3ab78745d620df0b97d92880059ed91c336a05
CaueGrassi <128317143+CaueGrassi7@users.noreply.github.com>
2024-07-26 15:01:31 -0300

    Creating payments page & dynamic values

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request, send_file, render_template
 from repository.database import db
 from db_models.payment import Payment
+from datetime import datetime, timedelta
+from payments.pix import Pix
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
@@ -10,7 +13,27 @@ db.init_app(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_pix_payment():
-    return jsonify({'message': 'Pix payment created!'})
+    data = request.get_json()
+
+    # Validations
+    if 'value' not in data:
+        return jsonify({'message': 'Value is required!'}), 400
+    
+    expiration_date = datetime.now() + timedelta(minutes=30)
+
+    new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    
+    new_payment.bank_payment_id = data_payment_pix['bank_payment_id']
+    new_payment.qr_code = data_payment_pix['qr_code_path']
+
+    db.session.add(new_payment)
+    db.session.commit()
+
+    return jsonify({'message': 'Pix payment created!', 
+                   "payment": new_payment.to_dict()})
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def confirm_pix_payment():
@@ -18,7 +41,18 @@ def confirm_pix_payment():
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def get_pix_payment(payment_id):
-    return jsonify({'payment_id': payment_id})
+    payment = Payment.query.get(payment_id)
+
+
+    return render_template('payment.html',
+                           payment_id=payment.id,
+                           value=payment.value,
+                           host='http://127.0.0.1:5000',
+                           qr_code=payment.qr_code)
+
+@app.route('/payments/pix/qr_code/<file_name>', methods=['GET'])
+def get_image(file_name):
+    return send_file(f'static/img/{file_name}.png', mimetype='image/png')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db_models/payment.py
+++ db_models/payment.py
@@ -5,6 +5,16 @@ class Payment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     value = db.Column(db.Float, nullable=False)
     paid = db.Column(db.Boolean, default=False)
-    bank_payment_id = db.Column(db.Integer, nullable=True)
+    bank_payment_id = db.Column(db.String(255), nullable=True)
     qr_code = db.Column(db.String(255), nullable=True)
     expiration_date = db.Column(db.DateTime, nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'value': self.value,
+            'paid': self.paid,
+            'bank_payment_id': self.bank_payment_id,
+            'qr_code': self.qr_code,
+            'expiration_date': self.expiration_date
+        }
