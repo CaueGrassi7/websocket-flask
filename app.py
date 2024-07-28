ab0f4c6ff3ce578ae76dbf0a8a8fdf0c5dd4444f
CaueGrassi <128317143+CaueGrassi7@users.noreply.github.com>
2024-07-28 16:48:31 -0300

    Finishing Websocket & alternative pages

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
 from payments.pix import Pix
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 
@@ -10,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SECRET_KEY'] = 'SECRET_KEY_WEBSOCKET'
 
 db.init_app(app)
+socketio = SocketIO(app)
 
 @app.route('/payments/pix', methods=['POST'])
 def create_pix_payment():
@@ -37,12 +39,41 @@ def create_pix_payment():
 
 @app.route('/payments/pix/confirmation', methods=['POST'])
 def confirm_pix_payment():
+    data = request.get_json()
+
+    # Validations
+    if 'bank_payment_id' not in data or 'value' not in data:
+        return jsonify({'message': 'Invalid payment data'}),400
+    # Payment
+    payment = Payment.query.filter_by(bank_payment_id=data.get('bank_payment_id')).first()
+
+    if not payment or payment.paid:
+        return jsonify({'message': 'Payment not found!'}), 404
+    
+    received_value = float(data.get('value'))
+    payment_value = float(payment.value)
+
+    if received_value != payment_value:
+        return jsonify({'message': 'Invalid payment data'}), 400
+
+    
+    payment.paid = True
+    db.session.commit()
+    socketio.emit(f'payment-confirmed-{payment.id}')
     return jsonify({'message': 'Pix payment confirmed!'})
 
 @app.route('/payments/pix/<int:payment_id>', methods=['GET'])
 def get_pix_payment(payment_id):
     payment = Payment.query.get(payment_id)
 
+    if not payment:
+        return render_template('404.html')
+
+    if payment.paid:
+        return render_template('confirmed_payment.html',
+                               payment_id=payment.id,
+                               value=payment.value
+                               )
 
     return render_template('payment.html',
                            payment_id=payment.id,
@@ -54,5 +85,14 @@ def get_pix_payment(payment_id):
 def get_image(file_name):
     return send_file(f'static/img/{file_name}.png', mimetype='image/png')
 
+# Websocket
+@socketio.on('connect')
+def handle_connnect():
+    print('Client connected!')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Client disconnected!')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app, debug=True)
